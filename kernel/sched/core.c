3c8eb0a4377f93f4f8e0a9de856fcf08d045995e
Syukri Lajin <ginger_boy92@hotmail.com>
2015-10-21 21:07:49 +0800

    fix build

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -746,8 +746,6 @@ static void enqueue_task(struct rq *rq, struct task_struct *p, int flags)
 	update_rq_clock(rq);
 	sched_info_queued(p);
 	p->sched_class->enqueue_task(rq, p, flags);
-	trace_sched_enq_deq_task(p, 1);
-	rq->cumulative_runnable_avg += p->se.ravg.demand;
 	inc_cumulative_runnable_avg(rq, p);
 }
 
@@ -756,7 +754,6 @@ static void dequeue_task(struct rq *rq, struct task_struct *p, int flags)
 	update_rq_clock(rq);
 	sched_info_dequeued(p);
 	p->sched_class->dequeue_task(rq, p, flags);
-	trace_sched_enq_deq_task(p, 0);
 	dec_cumulative_runnable_avg(rq, p);
 }
 
